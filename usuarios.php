68eb63de273479d1d636635c287f16730ebc08a2
jomafra <98652476+jomafra@users.noreply.github.com>
2022-04-12 18:29:25 -0500

    cambio usuarios

--- usuarios.php
+++ usuarios.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-session_start();
-error_reporting(0);
-$varsesion = $_SESSION['usuario'];
-$perfil    = $_SESSION['perfil'];
+    session_start();
+    error_reporting(0);
+        $varsesion = $_SESSION['usuario'];
+        $perfil    = $_SESSION['perfil'];
+    
 
-require_once("layout/cabecerahtml.php");
+    require_once("layout/cabecerahtml.php");
+    require_once("layout/header.php");
 ?>
 <body>
-<?php 
-require_once("layout/header.php");
-?>
     <div class="container"> 
-            <?php if ($perfil == 3){ 
-                    echo"<div class='row'>
-                    <div class='col-lg-3 mt-2'>";
-                
-                    require_once("layout/ladoizq.php");
-                    echo"</div>";
-                }
-                ?>                
-            
-            <div class="col-lg-9 mt-3">
-                <div class="row">
-                <?php
-                    require_once("formularios/fusuarios.php");
-                ?>                
-                </div>
+        <div class='row'>
+            <div class='col-lg-3 mt-2'>
+                <?php   
+                    if($perfil== 3){
+                        require_once("layout/ladoizq.php");
+                    }     
+                    
+                ?> 
+            </div>               
+            <div class="col-lg-1"></div>
+            <div class="col-lg-8 mt-2">
+                <?php require_once("formularios/fusuarios.php"); ?>
             </div>
         </div>
     </div> 
