dff4d8e0165c3270516a0a5de7c7749813366189
jomafra <98652476+jomafra@users.noreply.github.com>
2022-04-12 13:02:45 -0500

    correccion a header/validar

--- layout/header.php
+++ layout/header.php
@@ -1,7 +1,7 @@
 <header>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container">
-    <a class="navbar-brand" href="../validar.php">System's Event</a>
+    <a class="navbar-brand" href="validar.php">System's Event</a>
     <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
