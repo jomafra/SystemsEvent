73146445d3e636f415fc77afec5951dc99f5bbcd
jomafra <98652476+jomafra@users.noreply.github.com>
2022-04-20 16:15:25 -0500

    nuevos parametros de bd

--- registros/conec.php
+++ registros/conec.php
@@ -1,7 +1,8 @@
 <?php
 
-//$coneccion=mysqli_connect("localhost","root","","soseventbd");
-$coneccion = mysqli_connect ("sql10.freesqldatabase.com","sql10485124","ZDS7x7KlRv","sql10485124");
+//local : "localhost","root","","soseventbd"
+// base datos free: "sql10.freesqldatabase.com","sql10485124","ZDS7x7KlRv","sql10485124"
+$coneccion = mysqli_connect ("bqfdjm8lopw6a7ramwqv-mysql.services.clever-cloud.com","ukndex2jhjoaz9ty","MDPcGBt3htnulwxYY2ig","bqfdjm8lopw6a7ramwqv");
 if(!$coneccion){
     echo"-Algo pasoó- no se pudo conectar a base de datos";
 }
