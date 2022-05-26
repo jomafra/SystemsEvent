4c5b8581188303a76fce8438121ea9a243d868d0
jomafra <jose-fragozo@hotmail.com>
2022-05-26 16:40:54 -0500

    Update conec.php
    cambios en cadena de coneccion, por seguridad.

--- registros/conec.php
+++ registros/conec.php
@@ -1,8 +1,5 @@
 <?php
-
-//local : "localhost","root","","soseventbd"
-// base datos free: "sql10.freesqldatabase.com","sql10485124","ZDS7x7KlRv","sql10485124"
-$coneccion = mysqli_connect ("bqfdjm8lopw6a7ramwqv-mysql.services.clever-cloud.com","ukndex2jhjoaz9ty","MDPcGBt3htnulwxYY2ig","bqfdjm8lopw6a7ramwqv");
+$coneccion = mysqli_connect ("localhost","usuario","password","bd");
 if(!$coneccion){
     echo"-Algo pasoó- no se pudo conectar a base de datos";
 }
